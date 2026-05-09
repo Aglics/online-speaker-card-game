78b667cc2ec6d2f71bb45aa745f9ba9a6beda005
Aglics <734320909@qq.com>
2026-05-09 17:32:35 +0930

    Initial commit: setup

---
+++ src/components/PromptCard.jsx
@@ -0,0 +1,13 @@
+export default function PromptCard({ label, value, onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      className="cursor-pointer bg-white/5 hover:bg-white/10 transition rounded-xl p-3 border border-white/10"
+    >
+      <div className="text-[10px] text-gray-400">{label}</div>
+      <div className="text-sm font-semibold mt-1">
+        {value || "Click to generate"}
+      </div>
+    </div>
+  );
+}
